5d2b23e16c247c3b01f960dea38eed92daec4a07
giulia volpe <giuliavolpe@hotmail.it>
2023-06-13 19:42:01 +0200

    modificata grafica messaggio salvataggio

--- Library/resources/views/author/index.blade.php
+++ Library/resources/views/author/index.blade.php
@@ -11,7 +11,7 @@
 
                     <div id="success-message">
                         @if (session('success'))
-                            <h4 class="fw-bold bg-success opacity-75 text-white">{{ session('success') }}</h4>
+                            <h4 class="fw-bold bg-success p-2 text-dark bg-opacity-50 border border-success rounded">{{ session('success') }}</h4>
                         @endif
                     </div>
                     <h1 class="fw-light text-center">Autori</h1>
